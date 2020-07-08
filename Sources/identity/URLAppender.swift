3928e71abd2ccaaebe37471bbe3b1575f7e5fbff
Nick Porter <nporter@adobe.com>
2020-07-08 16:00:22 -0600

    Stub out tests

--- Sources/identity/URLAppender.swift
+++ Sources/identity/URLAppender.swift
@@ -1,14 +1,17 @@
-//
-//  URLAppender.swift
-//  AEPCore
-//
-//  Created by Nick Porter on 7/8/20.
-//  Copyright © 2020 Adobe. All rights reserved.
-//
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
 
 import Foundation
 
-fileprivate extension String {
+private extension String {
     func indexOf(char: Character) -> Int? {
         return firstIndex(of: char)?.utf16Offset(in: self)
     }
@@ -52,7 +55,7 @@ struct URLAppender {
         return modifiedUrl
     }
     
-    private static func generateVisitorIdPayload(configSharedState: [String: Any], analyticsSharedState: [String: Any], identityProperties: IdentityProperties) -> String? {
+    static func generateVisitorIdPayload(configSharedState: [String: Any], analyticsSharedState: [String: Any], identityProperties: IdentityProperties) -> String? {
         // append timestamp
         var theIdString = appendParameterToVisitorIdString(original: "", key: IdentityConstants.VISITOR_TIMESTAMP_KEY, value: String(Date().timeIntervalSince1970))
         // append mid
@@ -75,7 +78,7 @@ struct URLAppender {
         return urlFragment
     }
     
-    private static func appendParameterToVisitorIdString(original: String, key: String, value: String) -> String {
+    static func appendParameterToVisitorIdString(original: String, key: String, value: String) -> String {
         if key.isEmpty || value.isEmpty {
             return original
         }
