8db0c4b24f69787f2af16affec9ea31d505c4607
Nick Porter <nporter@adobe.com>
2020-07-13 11:18:03 -0600

    Define protocol for HitQueing

--- Sources/utility/HitQueue.swift
+++ Sources/utility/HitQueue.swift
@@ -11,23 +11,49 @@ governing permissions and limitations under the License.
 
 import Foundation
 
-protocol HitProcessor: class {
+/// Defines a type which can process a hit
+protocol Processable: class {
+    
+    /// Function that is invoked with a `DataEntity` and provides functionality for processing the hit
+    /// - Parameters:
+    ///   - entity: The `DataEntity` to be processed
+    ///   - completion: a closure to be invoked with `true` if processing was successful and should not be retried, false if processing the hit should be retried
     func processHit(entity: DataEntity, completion: (Bool) -> ())
 }
 
-class HitQueue {
+protocol HitQueuing {
+    /// Queues a `DataEntity` to be processed
+    /// - Parameters:
+    ///   - entity: the entity to be processed
+    @discardableResult
+    func queue(entity: DataEntity) -> Bool
+    
+    /// Puts the queue in non-suspended state and begins processing hits
+    func beginProcessing()
+    
+    /// Puts the queue in a suspended state and discontinues hit processing
+    func suspend()
+    
+    /// Removes all the persisted hits from the queue
+    func clear()
+}
+
+/// Provides functionality for asynchronous processing of hits in a synchronous manner while providing the ability to retry hits
+class HitQueue: HitQueuing {
     let dataQueue: DataQueue
-    weak var delegate: HitProcessor?
+    weak var delegate: Processable?
     
     private var suspended = true
-    private var processingHit = false // modified by two threads, invesitgate
+    private let queue = DispatchQueue(label: "com.adobe.mobile.hitqueue")
     
+    /// Creates a new `HitQueue` with the underlying `DataQueue` which is used to persist hits
+    /// - Parameter dataQueue: a `DataQueue` used to persist hits
     init(dataQueue: DataQueue) {
         self.dataQueue = dataQueue
     }
     
     @discardableResult
-    func queue(entity: DataEntity, event: Event, configSharedState: [String: Any]) -> Bool {
+    func queue(entity: DataEntity) -> Bool {
         let result = dataQueue.add(dataEntity: entity)
         if !suspended {
             processNextHit()
@@ -35,34 +61,40 @@ class HitQueue {
         
         return result
     }
-    
-    func bringOnline() {
+
+    func beginProcessing() {
         suspended = false
         processNextHit()
     }
-    
+
     func suspend() {
         suspended = true
     }
-    
+
     func clear() {
         let _ = dataQueue.clear()
     }
     
+    /// A recursive function for processing hits, it will continue processing all the hits
     private func processNextHit() {
         guard suspended else { return  }
-        guard !processingHit else { return } // ensure we are not currently processing a hit
         guard let hit = dataQueue.peek() else { return } // nothing let in the queue, stop processing
-        processingHit = true
         
-        delegate?.processHit(entity: hit, completion: { (success) in
-            processingHit = false
+        // Use a dispatch queue so we can wait for the hit to finish processing asynchronous before moving to the next hit
+        let group = DispatchGroup()
+        group.enter()
+        
+        delegate?.processHit(entity: hit, completion: { [weak self] (success) in
             if success {
                 // successful processing of hit, remove it from the queue, if failed leave in queue to be retried
-                let _ = dataQueue.remove()
+                let _ = self?.dataQueue.remove()
             }
             
-            processNextHit()
+            group.leave()
         })
+        
+        group.notify(queue: queue) {
+            self.processNextHit()
+        }
     }
 }
